de9e0cfafea0c83ebac2515316e1a515b67517c7
joseramorez <joseramorez.jr@gmail.com>
2023-03-04 18:50:14 -0600

    ok

--- src/app/invitacion/invitacion.component.ts
+++ src/app/invitacion/invitacion.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { PresentacionService } from '../services/presentacion.service';
+import * as confetti from 'canvas-confetti';
+
 
 @Component({
   selector: 'app-invitacion',
@@ -7,13 +9,31 @@ import { PresentacionService } from '../services/presentacion.service';
   styleUrls: ['./invitacion.component.css']
 })
 export class InvitacionComponent implements OnInit {
+  public clicked = false;
+  imagenes = ["img1","img2","img3","img4","img5","img6","img7","img8","img9","img10","img11","img12","img13","img14" ];
   constructor(
-    private _PresentacionService: PresentacionService
+    private _PresentacionService: PresentacionService,
+    private renderer2: Renderer2,
+    private elementRef: ElementRef
   ) {
     _PresentacionService.cargarjs(['cumple']);
+
   }
 
   ngOnInit() {
+    // this.surprise();
   }
 
+  public surprise(): void {
+
+    const canvas = this.renderer2.createElement('canvas');
+    this.renderer2.appendChild(this.elementRef.nativeElement, canvas);
+
+    const myConfetti = confetti.create(canvas, {
+      resize: true
+    });
+
+    myConfetti();
+    this.clicked = true;
+  }
 }
