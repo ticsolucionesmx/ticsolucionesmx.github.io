deacebc9374836b53513b865d2fb2e69a67a7c48
JOSE ANTONIO <antonio.ramirez@leon.gob.mx>
2024-01-08 13:22:40 -0600

    modificaciones recientes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,33 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import {aboutInterface, initAbout} from '../../interfaces/about.interface'
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  page = 'Presentacion';
-  nombre = 'Jose Antonio';
-  // datos navegador
-  home = 'inicio';
-  About = 'Hacerca de Mi';
-  Resume = 'Resumen';
-  Portfolio = 'Portafolio';
-  Services = 'Servicios';
-  Contact = 'Contacto';
-  // datos de contenido
-  Skills = 'Habilidades';
-  // hacerca de mi
-  Birthday = 'Cumpleaños';
-  _Brithday = "23 de Marzo"
-  Website = 'Website';
-  _Website = 'ticsolucionesmx.github.io';
-  Phone = 'Telefono';
-  _Phone = '951 350 62 72';
-  City = 'Ciudad';
-  _mail = 'joseramirez.jr2303@gmail.com';
-  Age = 'Edad';
-  edadcalculo = '23/03/1991';
+  public about: aboutInterface = initAbout;
   constructor () {}
 
 }
--- src/app/interfaces/about.interface.ts
+++ src/app/interfaces/about.interface.ts
@@ -1,27 +1,42 @@
 export interface aboutInterface {
-  About: string ;
-  Birthday: string ;
+  page: string
+  nombre: string
+  home: string;
+  Resume: string
+  About: string;
+  Portfolio: string;
+  Services: string;
+  Birthday: string;
   _Brithday: string;
-  Website: string ;
-  _Website: string ;
-  Phone: string ;
-  _Phone: string ;
-  City: string ;
-  _mail: string ;
+  Website: string;
+  _Website: string;
+  Phone: string;
+  _Phone: string;
+  City: string;
+  _mail: string;
   Age: string;
-  edadcalculo: string ;
+  edadcalculo: string;
 }
 
-export const initAbout: aboutInterface =  {
-  About:  'Hacerca de Mi',
-  Birthday : 'Cumpleaños',
-  _Brithday : "23 de Marzo",
-  Website : 'Website',
-  _Website : 'ticsolucionesmx.github.io',
-  Phone : 'Telefono',
-  _Phone : '951 350 62 72',
-  City : 'Ciudad',
-  _mail : 'joseramirez.jr2303@gmail.com',
-  Age : 'Edad',
-  edadcalculo : '23/03/1991',
+export const initAbout: aboutInterface = {
+  page: 'Presentación',
+  nombre: 'Jose Antonio',
+  // datos navegador
+  home: 'inicio',
+  About: 'Hacerca de Mi',
+  Resume: 'Resumen',
+  Portfolio: 'Portafolio',
+  Services: 'Servicios',
+  // datos de contenido
+  // hacerca de mi
+  Birthday: 'Cumpleaños',
+  _Brithday: "23 de Marzo",
+  Website: 'Website',
+  _Website: 'ticsolucionesmx.github.io',
+  Phone: 'Telefono',
+  _Phone: '951 350 62 72',
+  City: 'Ciudad',
+  _mail: 'joseramirez.jr2303@gmail.com',
+  Age: 'Edad',
+  edadcalculo: '23/03/1991'
 }
